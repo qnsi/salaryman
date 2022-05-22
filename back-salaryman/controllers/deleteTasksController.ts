f9a778368f09475a03c76c53a6a84e65ef79f203
qnsi <arturkesik@gmail.com>
2022-05-22 12:58:07 +0200

    Fix order when deleting, fix adding state on new task

--- back-salaryman/controllers/deleteTasksController.ts
+++ back-salaryman/controllers/deleteTasksController.ts
@@ -1,12 +1,59 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma, PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
 export default async function deleteTask(req: Request, res: Response, prisma: PrismaClient) {
   const id = req.body.task.id
+  const task = await findTask(id, prisma)
+  if (task) {
+    await fixOrderAfterDeletion(task.parentId, task.order, prisma)
+    await deleteTaskFromDB(id, prisma)
+    res.json({status: "ok"})
+  } else {
+    res.json({status: "error"})
+  }
+}
+
+async function findTask(id: number, prisma: PrismaClient) {
+  return await prisma.task.findUnique({
+    where: {
+      id
+    }
+  })
+}
+
+async function deleteTaskFromDB(id: number, prisma: PrismaClient) {
   await prisma.task.delete({
     where: {
       id
     }
   })
-  res.json({status: "ok"})
+}
+
+async function fixOrderAfterDeletion(parentId: number | null, deletedTaskOrder: number, prisma: PrismaClient) {
+  const siblings = await findSiblings(parentId, prisma)
+  for (const sibling of siblings) {
+    if (sibling.order > deletedTaskOrder) {
+      await updateOrder(sibling.id, sibling.order, prisma)
+    }
+  }
+}
+
+async function findSiblings(parentId: number | null, prisma: PrismaClient) {
+  return await prisma.task.findMany({
+    where: {
+      parentId
+    }
+  })
+}
+
+async function updateOrder(id: number, order: number, prisma: PrismaClient) {
+  order = order - 1
+  await prisma.task.update({
+    where: {
+      id
+    },
+    data: {
+      order
+    }
+  })
 }
