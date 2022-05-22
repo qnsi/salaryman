ab6bc70de1902d2500784d53272ff20fce81cb23
qnsi <arturkesik@gmail.com>
2022-05-22 11:15:04 +0200

    Clean css a bit

--- front-salaryman/src/components/Task.tsx
+++ front-salaryman/src/components/Task.tsx
@@ -1,18 +1,44 @@
+import React from "react";
 import { TaskType } from "./TaskView";
 
 export default function Task(props: {task: TaskType, setAddingSubtaskId: Function, deleteTask: Function, collapseTask: Function}) {
-  var collapsedCharacter = "-"
+  const [hovered, setHovered] = React.useState(false)
+
+  var collapsedCharacter = "- "
   if (props.task.collapsed) {
-    collapsedCharacter = "+"
+    collapsedCharacter = "+ "
+  }
+  var intendationArray = Array.from({length: props.task.intendation}, Math.random)
+
+  var buttons = <></>
+  if (hovered) {
+    buttons = (
+      <div className="task-buttons">
+        <button onClick={() => {props.setAddingSubtaskId(props.task.id)}}>Add Subtask</button>
+        <button onClick={() => {props.deleteTask(props.task.id)}}>Delete</button>
+      </div>
+    )
   }
 
   return (
-    <div className="task" key={props.task.id} style={{margin: `0 0 0 ${props.task.intendation * 10}px`}} >
-      <span onClick={() => props.collapseTask(props.task)}>{collapsedCharacter}</span>
-      <span>{props.task.text}</span>
-      {/* <button onClick={() => {props.editTask}}>Edit</button> */}
-      <button onClick={() => {props.setAddingSubtaskId(props.task.id)}}>Subtask</button>
-      <button onClick={() => {props.deleteTask(props.task.id)}}>Delete</button>
+    <div
+     onMouseEnter={()=>setHovered(true)}
+     onMouseLeave={()=>setHovered(false)}
+     className="task-container"
+    >
+      {intendationArray.map(() => {
+        return (
+          <div className="task-line-container">
+            <div className="task-line-border"></div>
+          </div>
+        )
+      })}
+      <div className="task" key={props.task.id} >
+        <span onClick={() => props.collapseTask(props.task)}>{collapsedCharacter}</span>
+        <span>{props.task.text}</span>
+        <span className="task-spacer"></span>
+        {buttons}
+      </div>
     </div>
   )
 }
