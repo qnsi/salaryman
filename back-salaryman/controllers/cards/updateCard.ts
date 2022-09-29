ab70fd23bcc17c2c45de6f9827d5914200477e10
qnsi <arturkesik@gmail.com>
2022-09-29 12:43:57 +0200

    Start working on moving card inside stage

--- back-salaryman/controllers/cards/updateCard.ts
+++ back-salaryman/controllers/cards/updateCard.ts
@@ -1,4 +1,4 @@
-import { Card, Prisma, PrismaClient } from "@prisma/client"
+import { Card, PrismaClient } from "@prisma/client"
 import { Request, Response } from "express"
 
 export async function updateCard(req: Request, res: Response, userId: number, prisma: PrismaClient){
@@ -11,10 +11,114 @@ export async function updateCard(req: Request, res: Response, userId: number, pr
   })
   if (guardAgainstNotFoundAndNotAuthorized(cardToBeUpdated, userId, res)) { return }
 
-  const card = await dropCardAtNewStageAtTheEnd(req.body.card.stage, req.body.card.id, userId, prisma)
-  decreaseOrderFromOriginalStage(cardToBeUpdated!.stage, cardToBeUpdated!.order, userId, prisma)
+  var card
+  if (cardToBeUpdated!.stage === req.body.card.stage) {
+    card = updateOrder(req.body.card.order, cardToBeUpdated!, userId, prisma)
+  } else {
+    card = updateStage(req.body.card.stage, cardToBeUpdated!, userId, prisma)
+  }
 
   res.json({card: card, status: "ok"})
+}
+
+async function updateOrder(newOrder: number, cardToBeUpdated: Card, userId: number, prisma: PrismaClient) {
+  await setTempOrderLargerThanLength(cardToBeUpdated, userId, prisma)
+  console.log("updateOrder")
+  console.log(newOrder, cardToBeUpdated)
+  if (newOrder > cardToBeUpdated.order) {
+    // for every card gt cardToBeUpdated.order ltequal newOrder
+    // order decrement
+    const cards = await prisma.card.updateMany({
+      where: {
+        order: {
+          gt: cardToBeUpdated.order,
+          lte: newOrder,
+        },
+        stage: cardToBeUpdated.stage
+      },
+      data: {
+        order: {
+          decrement: 1
+        }
+      }
+    })
+  } else {
+    // for every card gtequal newOrder lt cardToBeUpdated.order
+    // order increment
+    // const cards = await prisma.card.updateMany({
+    //   where: {
+    //     order: {
+    //       gte: newOrder, // 1
+    //       lt: cardToBeUpdated.order // 4
+    //     },
+    //     stage: cardToBeUpdated.stage
+    //   },
+    //   data: {
+    //     order: {
+    //       increment: 1
+    //     }
+    //   }
+    // })
+    const cards = await prisma.card.findMany({
+      where: {
+        userId,
+        stage: cardToBeUpdated.stage,
+        order: {
+          gte: newOrder,
+          lt: cardToBeUpdated.order
+        }
+      }
+    })
+    cards.sort((a, b) => b.order - a.order).forEach((card) => {
+      console.log("CARD")
+      console.log(card)
+      prisma.card.update({
+        where: {
+          id: card.id
+        }, 
+        data: {
+          order: card.order + 1
+        }
+      }).then(newCard => {
+        console.log("CARD UPDATED")
+        console.log(newCard)
+      })
+    })
+  }
+  // set cardToBeUpdated order to newOrder
+  await prisma.card.update({
+    where: {
+      id: cardToBeUpdated.id
+    }, 
+    data: {
+      order: newOrder
+    }
+  })
+}
+
+async function setTempOrderLargerThanLength(cardToBeUpdated: Card, userId: number, prisma: PrismaClient) {
+    const cardsLength = (await prisma.card.findMany({
+      where: {
+        userId,
+        stage: cardToBeUpdated.stage
+      }
+    })).length
+
+    // set cardToBeUpdated order to length+1 temporarly
+    await prisma.card.update({
+      where: {
+        id: cardToBeUpdated.id
+      }, 
+      data: {
+        order: cardsLength + 1
+      }
+    })
+}
+
+async function updateStage(stage: string, cardToBeUpdated: Card, userId: number, prisma: PrismaClient) {
+  const card = await dropCardAtNewStageAtTheEnd(stage, cardToBeUpdated.id, userId, prisma)
+  decreaseOrderFromOriginalStage(cardToBeUpdated!.stage, cardToBeUpdated!.order, userId, prisma)
+  return card
 }
 
 function guardAgainstNotFoundAndNotAuthorized(cardToBeUpdated: Card | null, userId: number, res: Response) {
--- front-salaryman/src/views/Kanban/Kanban.tsx
+++ front-salaryman/src/views/Kanban/Kanban.tsx
@@ -15,9 +15,12 @@ export default function Kanban() {
   var initCards: KanbanCard[] = []
 
   const [cards, setCards] = React.useState(initCards)
-  const optionsCards = cards.filter((card: KanbanCard) => card.stage === "options")
-  const doingCards = cards.filter((card: KanbanCard) => card.stage === "doing")
-  const doneCards = cards.filter((card: KanbanCard) => card.stage === "done")
+
+  function filteredCards(stage: string) {
+    return cards.filter((card: KanbanCard) => card.stage === stage)
+  }
+
+  const [draggedCardId, setDraggedCardId] = React.useState(0)
 
   React.useEffect(() => {
       getCardsFromBackend().then((response) => {
@@ -27,11 +30,34 @@ export default function Kanban() {
       })
   }, [])
 
-  function updateCardStage(id: number, stage: string) {
-    updateCard(id, stage).then((response) => {
+  function updateCardStage(stage: string) {
+    // const draggedCard = cards.find(card => card.id === draggedCardId) as KanbanCard
+    // if (draggedCard.stage === stage) {
+    //   updateOrder(stage)
+    // } else {
+      updateStage(stage)
+    // }
+  }
+
+  function updateCardOrder(stage: string, cardId: number) {
+    const draggedCard = cards.find(card => card.id === draggedCardId) as KanbanCard
+    const droppedCard = cards.find(card => card.id === cardId) as KanbanCard
+    if (draggedCard.stage !== stage) {
+      updateStage(stage)
+    } else {
+      updateCard(draggedCardId, stage, droppedCard.order).then((response) => {
+
+      }).catch(error => {
+        window.alert("We couldn't connect to the server! Try again.\n\n" + error)
+      })
+    }
+  }
+
+  function updateStage(stage: string) {
+    updateCard(draggedCardId, stage, 0).then((response) => {
       setCards((prevState: KanbanCard[]) => {
         return prevState.map((card: KanbanCard) => {
-          if (card.id === id) {
+          if (card.id === draggedCardId) {
             return {...card, stage}
           }
           return card
@@ -42,45 +68,83 @@ export default function Kanban() {
     })
   }
 
+  function updateCardOrderWhileDragging(draggedOverCardId: number) {
+    console.log("updateCardOrderWHielDragigin")
+    console.log(draggedCardId, draggedOverCardId)
+    console.log(cards)
+    const draggedCard = cards.find(card => card.id === draggedCardId) as KanbanCard
+    const draggedOverCard = cards.find(card => card.id === draggedOverCardId) as KanbanCard
+
+    setCards((prevState: KanbanCard[]) => {
+      const nextState =  prevState.map(card => {
+        if (card.id == draggedCardId) {
+          return {...card, order: draggedOverCard.order}
+        }
+        if (card.id == draggedOverCardId) {
+          return {...card, order: draggedCard.order}
+        }
+        return card
+      })
+      // its not working because the vars are not observing state. 
+      console.log(nextState)
+      return nextState
+    })
+  }
+
   return (
     <div className="grow basis-11/12 flex">
       <Spacer />
 
-      <CardGrouper name="options" cards={optionsCards} updateCardStage={updateCardStage}>
+      <CardGrouper name="options" cards={filteredCards("options")} updateCardStage={updateCardStage} updateCardOrder={updateCardOrder} updateCardOrderWhileDragging={updateCardOrderWhileDragging} setDraggedCardId={setDraggedCardId}>
         <AddNewCard setCards={setCards} />
       </CardGrouper>
 
       <Spacer />
 
-      <CardGrouper name="doing" cards={doingCards} updateCardStage={updateCardStage}>
+      <CardGrouper name="doing" cards={filteredCards("doing")} updateCardStage={updateCardStage} updateCardOrder={updateCardOrder} updateCardOrderWhileDragging={updateCardOrderWhileDragging} setDraggedCardId={setDraggedCardId}>
         <div></div>
       </CardGrouper>
 
       <Spacer />
 
-      <CardGrouper name="done" cards={doneCards} updateCardStage={updateCardStage}>
+      <CardGrouper name="done" cards={filteredCards("done")} updateCardStage={updateCardStage} updateCardOrder={updateCardOrder} updateCardOrderWhileDragging={updateCardOrderWhileDragging} setDraggedCardId={setDraggedCardId}>
         <div></div>
       </CardGrouper>
     </div>
   )
 }
 
-function CardGrouper(props: {name: string, cards: KanbanCard[], updateCardStage: Function, children: ReactElement}) {
+function CardGrouper(props: {name: string, cards: KanbanCard[], updateCardStage: Function, updateCardOrder: Function, updateCardOrderWhileDragging: Function, setDraggedCardId: Function, children: ReactElement}) {
   function handleDragStart(e: React.DragEvent<HTMLDivElement>, card: KanbanCard) {
     e.currentTarget.style.opacity = '0.4';
     e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData("text", String(card.id))
+    props.setDraggedCardId(card.id)
   }
   
   function handleDragEnd(e: React.DragEvent<HTMLDivElement>) {
     e.currentTarget.style.opacity = '1';
+    props.setDraggedCardId(0)
   }
 
   function handleDrop(e: React.DragEvent<HTMLDivElement>) {
     e.stopPropagation();
-    const id = Number(e.dataTransfer.getData("text"))
-    props.updateCardStage(id, props.name)
+    props.updateCardStage(props.name)
     return false;
+  }
+
+  function handleDropOnCard(e: React.DragEvent<HTMLDivElement>, card: KanbanCard) {
+    e.stopPropagation();
+    props.updateCardOrder(props.name, card.id)
+    return false;
+  }
+
+  function handleDragEnter(e: React.DragEvent<HTMLDivElement>, draggedOverCard: KanbanCard) {
+    props.updateCardOrderWhileDragging(draggedOverCard.id)
+  }
+
+  function handleDragOver(e: React.DragEvent<HTMLDivElement>, draggedOverCard: KanbanCard) {
+    e.preventDefault()
+    return false
   }
 
   return (
@@ -88,13 +152,16 @@ function CardGrouper(props: {name: string, cards: KanbanCard[], updateCardStage:
       <div className="justify-center flex text-xl">
         {props.name}
       </div>
-      {props.cards.map((card: KanbanCard) => {
+      {props.cards.sort((cardA, cardB) => cardA.order - cardB.order).map((card: KanbanCard) => {
         return (
           <div 
             className="bg-slate-100 m-1 p-1 cursor-move"
             draggable={true}
+            onDragOver={(e: React.DragEvent<HTMLDivElement>) => handleDragOver(e, card)}
             onDragStart={(e: React.DragEvent<HTMLDivElement>) => handleDragStart(e, card)}
             onDragEnd={handleDragEnd}
+            onDrop={(e: React.DragEvent<HTMLDivElement>) => handleDropOnCard(e, card)}
+            onDragEnter={(e: React.DragEvent<HTMLDivElement>) => handleDragEnter(e, card)}
             key={card.id}
           >
             {card.text}
