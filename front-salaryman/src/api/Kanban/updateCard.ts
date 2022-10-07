289d31a9a39177a603d010be26adc91baf902fec
qnsi <arturkesik@gmail.com>
2022-10-07 11:32:49 +0200

    Add option to update card text, filter by text

--- front-salaryman/src/api/Kanban/updateCard.ts
+++ front-salaryman/src/api/Kanban/updateCard.ts
@@ -7,3 +7,7 @@ const UPDATE_CARD_URL = "card/update"
 export function updateCard(id: number, stage: string, order: number): Promise<AxiosResponse<{status: string, card: KanbanCard}>> {
   return api.post<{status: string, card: KanbanCard}>(UPDATE_CARD_URL, {card: {id, stage, order}})
 }
+
+export function updateCardTextInBackend(id: number, text: string): Promise<AxiosResponse<{status: string, card: KanbanCard}>> {
+  return api.post<{status: string, card: KanbanCard}>(UPDATE_CARD_URL, {card: {id, text}})
+}
